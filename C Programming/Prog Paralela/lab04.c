26c3f67e50755d3c4b17d187908122a783d3ffaf
JulioBBL <julio_bbl@hotmail.com>
2017-10-13 22:43:28 -0300

    x

--- C Programming/Prog Paralela/lab04.c
+++ C Programming/Prog Paralela/lab04.c
@@ -12,36 +12,34 @@
 #include <omp.h>
 
 int main() {
-    unsigned long size = 0x1 << 31;
+    unsigned long size = 0x1 << 16;
     clock_t begin, end;
-    
+
     int *array1 = (int*)calloc(size, sizeof(int));
     int *array2 = (int*)calloc(size, sizeof(int));
     int *array3 = (int*)calloc(size, sizeof(int));
     int sum1 = 0;
     int sum2 = 0;
     int sum3 = 0;
-    
+
     for (int i = 0; i < size; i++) {
-        array1[i] = i;
-        array2[i] = i;
-        array3[i] = i;
+        array1[i] = i+1;
+        array2[i] = i+1;
+        array3[i] = i+1;
     }
-    
+
     int max = omp_get_max_threads();
     int chunk = size / max;
     int tempArray[max];
-    
-    printf("max threads: %d\n", max);
-    
+
+    printf("max threads: %d - array size: %ld\n", max, size);
+
     begin = clock();
 #pragma omp parallel
     {
-        int thread_id = omp_get_thread_num();
-        
         for (int i = 0; i < chunk; i++)
         {
-            tempArray[thread_id] += array1[(chunk * thread_id)+i];
+            tempArray[omp_get_thread_num()] += array1[(chunk * omp_get_thread_num())+i];
         }
     }
     for (int i = 0; i < max; i++) {
@@ -49,34 +47,28 @@ int main() {
     }
     end = clock();
     printf("1. %f tics - sum: %d\n", (double)(end - begin), sum1);
-    
+
     begin = clock();
 #pragma omp parallel
     {
-        int thread_id = omp_get_thread_num();
-        
         for (int i = 0; i < chunk; i++)
         {
 #pragma omp critical
             {
-                sum2 += array2[(chunk * thread_id)+i];
+                sum2 += array2[(chunk * omp_get_thread_num())+i];
             }
         }
     }
     end = clock();
     printf("2. %f tics - sum: %d\n", (double)(end - begin), sum2);
-    
+
     begin = clock();
-#pragma omp parallel for private(sum3) reduction(+ : sum3)
-    {
-        int thread_id = omp_get_thread_num();
-        
-        for (int i = 0; i < chunk; i++) {
-            sum3 += array3[(chunk * thread_id)+i];
-        }
-    }
+#pragma omp parallel for reduction(+ : sum3)
+      for (int i = 0; i < chunk; i++) {
+          sum3 += array3[(chunk * omp_get_thread_num())+i];
+      }
     end = clock();
     printf("3. %f tics - sum: %d\n", (double)(end - begin), sum3);
-    
+
     return 0;
 }
